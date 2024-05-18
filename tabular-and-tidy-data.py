f9252e0e9499c26cc285e262a667276c9157e8f0
MFB-Napier <40620574@live.napier.ac.uk>
2024-05-18 22:29:44 +0200

    datasets commit, adding initial files

---
+++ tabular-and-tidy-data.py
@@ -0,0 +1,12 @@
+#Declare variable
+house_0_list=[115910.26,128,4]
+
+#print objeto
+print("house_0_list_length", type(house_0_list))
+
+print("house_0_list_length:", len(house_0_list))
+
+house_0_list
+
+
+
